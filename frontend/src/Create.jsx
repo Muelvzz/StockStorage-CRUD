d314e10ea2809e6eb7b9a54907b8f01bf2be2e16
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-12-08 17:48:09 +0800

    finished coding the "add inventory"

--- frontend/src/Create.jsx
+++ frontend/src/Create.jsx
@@ -1,12 +1,56 @@
 import "./create.css"
 import "./dashboard.css"
 
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { Link } from "react-router-dom"
 
 export default function Create() {
 
     const [toggleCreate, setToggleCreate] = useState(false)
+
+    const [category, setCategory] = useState("")
+    const [name, setName] = useState("")
+    const [stock, setStock] = useState(0)
+    const [image, setImage] = useState("")
+    const [notes, setNotes] = useState("")
+
+    const fileRef = useRef()
+
+    const handleCategory = (e) => {
+        setCategory(e.target.value)
+    }
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
+    const handleStock = (e) => {
+        setStock(e.target.value)
+    }
+
+    const handleImage = (e) => {
+        setImage(e.target.files[0])
+    }
+
+    const handleNotes = (e) => {
+        setNotes(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (category !== "") {
+            setName("")
+            setStock(0)
+            setImage("")
+            setNotes("")
+
+            setToggleCreate(false)
+        } else {
+            setToggleCreate(false)
+            window.alert("please fill the category above first")
+        }
+    }
 
     return (
         <>
@@ -28,13 +72,84 @@ export default function Create() {
             </nav>
 
             <main id="main-create-overlay">
+
+                <div id="main-create-header">
+                    <input 
+                        type="text" 
+                        placeholder="Enter the Category Name"
+                        id="category-btn"
+                        value={category}
+                        onChange={handleCategory}
+                    />
+                </div>
+
                 <div id="main-create-content">
 
-                    <button id="add-card-overlay">
+                    <button id="add-card-overlay" onClick={() => setToggleCreate(true)}>
                         <h1>Add +</h1>
                     </button>
 
                 </div>
+
+                { toggleCreate && (
+                    <div id="modal-overlay">
+
+                        <div id="modal-content">
+                            <div id="modal-header">
+                                <h2>Add Inventory</h2>
+                                <button id="cancel-btn"  onClick={() => setToggleCreate(false)}>Cancel</button>
+                            </div>
+
+                            <hr />
+
+                            <form onSubmit={handleSubmit} id="modal-form">
+                                <div id="modal-input">
+                                    <h3>Name:</h3>
+                                    <input 
+                                        type="text"
+                                        placeholder="Enter the Inventory Name"
+                                        value={name}
+                                        onChange={handleName}
+                                        required
+                                    />
+                                </div>
+                                <div id="modal-input">
+                                    <h3>Stock:</h3>
+                                    <input 
+                                        type="number"
+                                        placeholder="Enter the number of stocks"
+                                        value={stock}
+                                        onChange={handleStock}
+                                        required
+                                    />
+                                </div>
+                                <div id="modal-input">
+                                    <h3>Image:</h3>
+                                    <input 
+                                        type="file"
+                                        ref={fileRef}
+                                        onChange={handleImage}
+                                        required
+                                    />
+                                </div>
+                                <textarea
+                                    rows={5}
+                                    placeholder="Enter your notes (Optional)"
+                                    value={notes}
+                                    onChange={handleNotes}
+                                ></textarea>
+
+                                <button 
+                                    id="add-btn" 
+                                    onClick={() => {handleSubmit}}
+                                >Add</button>
+                            </form>
+
+                        </div>
+
+                    </div>
+                )}
+
             </main>
 
         </>
